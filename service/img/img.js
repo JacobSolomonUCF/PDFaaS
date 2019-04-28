1c81fcdb3235abcab5a4d2dc085af5b55720da93
Jacob Solomon <j.solomon@knights.ucf.edu>
2019-04-28 17:56:21 -0400

    Added getScreenshot

--- service/img/img.js
+++ service/img/img.js
@@ -1,17 +1,51 @@
-const { openBrowser, loadPage } = require("chromiumBase");
+const { openBrowser, loadPage, getScreenshot } = require("chromiumBase");
+/*
+
+{
+  html: <String> # HTML string of the content
+  url: <String> # URL of the content
+}
+
+*/
 
 exports.imgHandler = async (event, context, callback) => {
-  const page = await openBrowser();
+  const response = {};
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'HELLO from IMG',
-      input: event,
-    }),
-  };
+  if (event.body !== null && event.body !== undefined) {
+    const browser = await openBrowser();
+    const body = JSON.parse(event.body);
 
-  page.close();
+    // Defaults for now
+    const media = 'screen';
+    const screenSize = {
+      width: 1920,
+      height: 1080,
+    };
+
+    let asHTML = false;
+    var content = '';
+    if(body.html && body.html !== ''){
+      asHTML = true;
+      content = body.html
+    }else{
+      content = body.url
+    }
+    const page = await loadPage(browser, content, media, screenSize, asHTML);
+    const screenShot = await getScreenshot(page);
+
+    response.statusCode = 200;
+    response.body = JSON.stringify({
+      message: 'Success',
+      content: screenShot
+    });
+    browser.close();
+  }else{
+    response.statusCode = 400;
+    response.body = JSON.stringify({
+      message: 'No data in body',
+      content: null,
+    });
+  }
 
   callback(null, response);
 };
